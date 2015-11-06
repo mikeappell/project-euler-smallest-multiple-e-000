256c8718e469bb898b8bd265f9f6c5447b1dff42
mikeappell <mikeappell@gmail.com>
2015-11-06 05:44:26 +0000

    Done.

--- lib/oo_smallest_multiple.rb
+++ lib/oo_smallest_multiple.rb
@@ -1,1 +1,14 @@
-# Implement your object-oriented solution here!
+class SmallestMultiple
+  def initialize(num)
+    @num = num
+  end
+
+  def lcm
+    (@num..Float::INFINITY).step(@num).each do |multiple|
+      (2..@num).each do |div|
+        break unless multiple % div == 0
+        return multiple if div == @num
+      end
+    end
+  end
+end
--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -1,1 +1,8 @@
-# Implement your procedural solution here!
+def smallest_multiple(num)
+  (num..Float::INFINITY).step(num).each do |multiple|
+    (2..num).each do |div|
+      break unless multiple % div == 0
+      return multiple if div == num
+    end
+  end
+end
